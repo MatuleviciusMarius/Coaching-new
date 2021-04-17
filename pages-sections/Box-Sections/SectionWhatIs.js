5763f7633e3ff0d9ce6d3b73ef3ef712b9d1b515
Marius <marius.matulevicius1@gmail.com>
2021-04-17 17:16:45 +0300

    Text markup update

--- pages-sections/Box-Sections/SectionWhatIs.js
+++ pages-sections/Box-Sections/SectionWhatIs.js
@@ -46,7 +46,9 @@ export default function SectionPradekime() {
             aria-labelledby='classic-modal-slide-title'
             aria-describedby='classic-modal-slide-description'>
             <DialogTitle id='classic-modal-slide-title' disableTypography className={classes.modalHeader}>
-              <h4 className={classes.modalTitle}>Paauglių ir Jaunimo koučingas</h4>
+              <h4 className={classes.modalTitle}>
+                <b>Paauglių ir Jaunimo koučingas </b>
+              </h4>
             </DialogTitle>
             <DialogContent id='classic-modal-slide-description' className={classes.modalBody}>
               <p>
@@ -55,40 +57,45 @@ export default function SectionPradekime() {
                 nuo gero gyvenimo pereiti prie puikaus?
               </p>
               <p>
-                Kas yra paauglių ir jaunimo koučingas? Tėvai pas paauglių koučingo specialistus kreipiasi dėl
-                įvairiausių priežasčių, tačiau esmė visuomet išlieka ta pati, tai pokyčiai.
-              </p>
-              <p>Kaip vyksta pokyčiai gyvenime, kaip pagerinti bendrą laimės ir gerovės lygį?</p>
-              <p>
-                Koučingas įgalina: <br />
-                <span className={classes.paddingLeft}>
-                  - praplėsti SAVO vizijas ir galimybes, <br />- daryti proveržį SAVO gyvenime, <br />
-                  - nustatyti SAVO tikslus, <br />
-                  - numatyti SAVO galimas kliūtis siekiant SAVO tikslų, <br />
-                  - susikurti SAVO veiksmų planą, <br />
-                  - susidėlioti SAVO prioritetus, <br />
-                  - išsiaiškinti SAVO vertybes, <br />
-                  - rasti SAVO kursą į SAVO sėkmę, <br />- siekti SAVO svajonių.
-                </span>
+                <b>Kas yra paauglių ir jaunimo koučingas? </b> <br />
+                Tėvai pas paauglių koučingo specialistus kreipiasi dėl įvairiausių priežasčių, tačiau esmė visuomet
+                išlieka ta pati, tai <b>pokyčiai. </b>
               </p>
               <p>
-                Kas savaitę vykstančių koučingo sesijų metu koučeris padeda atrakinti jaunuolio/ės viduje slypinčią jėgą
-                ir galią. Koučeris aktyviai klauso ir užduoda klausimus, kurie padeda sukurti naują perspektyvą ir
-                įžvalgas. Koučeris talkina apmąstant pirmuosius žingsnius kaip pasirinkus nenukrypstant eiti
-                pasirinktuoju keliu, atrasti sėkmės įrankius, padeda atskleisti paaugliuose esančias neribotas
-                galimybes. Sesijų metu sužinoma apie kliūtis, trukdančias pasiekti tikslų, aptariami galimi žingsniai,
-                kurie padės jas pašalinti arba jų išvengti.{' '}
+                <b>Kaip vyksta pokyčiai gyvenime, kaip pagerinti bendrą laimės ir gerovės lygį? </b>
               </p>
               <p>
-                Kas nėra koučingas? <br />
+                <b>Koučingas įgalina:</b> <br />
+                <p className={classes.paddingLeft}>
+                  - praplėsti <b>savo</b> vizijas ir galimybes, <br />
+                  - daryti proveržį <b>savo</b> gyvenime, <br />
+                  - nustatyti <b>savo</b> tikslus, <br />
+                  - numatyti <b>savo</b> galimas kliūtis siekiant SAVO tikslų,<br />
+                  - susikurti <b>savo</b> veiksmų planą, <br />
+                  - susidėlioti <b>savo</b> prioritetus, <br />
+                  - išsiaiškinti <b>savo</b> vertybes, <br />
+                  - rasti <b>savo</b> kursą į savo sėkmę, <br />
+                  - siekti <b>savo</b> svajonių.
+                </p>
+              </p>
+              <p>
+                Kas savaitę vykstančių koučingo sesijų metu koučeris padeda
+                <b>atrakinti jaunuolio/ės viduje slypinčią jėgą ir galią.</b> Koučeris aktyviai
+                <b>klauso ir užduoda klausimus</b>, kurie padeda sukurti naują perspektyvą ir įžvalgas. Koučeris talkina
+                apmąstant pirmuosius žingsnius kaip pasirinkus nenukrypstant eiti pasirinktuoju keliu, atrasti sėkmės
+                įrankius, padeda atskleisti paaugliuose esančias neribotas galimybes. Sesijų metu sužinoma apie kliūtis,
+                trukdančias pasiekti tikslų, aptariami galimi žingsniai, kurie padės jas pašalinti arba jų išvengti.
+              </p>
+              <p>
+                <b>Kas nėra koučingas?</b> <br />
                 Koučingas nėra terapija, psichoterapija ar konsultavimas. Koučingas orientuotas į jaunimo stipriąsias
                 puses, o ne į silpnąsias, orientuotas į ateitį, o ne į praeitį, priešingai nei terapijoje. Koučingas tai
                 visuomet mąstymas į priekį, planai ir veiksmai į ateitį. Priešingai nei konsultavime, koučeris nepasako
                 ką daryti, o ko nedaryti, neduoda “teisingų“ atsakymų nei jaunuoliams, nei jų tėvams.
               </p>
               <p>
-                Paauglių ir jaunimo koučingas: <br />
-                <span className={classes.paddingLeft}>
+                <b>Paauglių ir jaunimo koučingas:</b> <br />
+                <div className={classes.paddingLeft}>
                   - patikima investicija į Jūsų paauglio sėkmingesnę ir laimingesnę ateitį, <br />
                   - idealiai tinka paaugliams ir jauniems 14-21 metų žmonėms, <br />
                   - reguliarūs kassavaitiniai skambučiai arba susitikimai, <br />
@@ -96,9 +103,9 @@ export default function SectionPradekime() {
                   - pirmasis pokalbis nemokamas, <br />
                   - koučingo sesijų ciklas: 12 kassavaitinių sesijų per 3 mėnesius, <br />
                   - viena sesija 45 minutės. <br />
-                </span>
+                </div>
                 <span>
-                  Pateikite savo klausimus, rūpesčius, užklausą. <br />
+                  <b>Pateikite savo klausimus, rūpesčius, užklausą.</b> <br />
                   Atskleiskime sėkmingo ir laimingo jaunimo paslaptis.
                 </span>
               </p>
